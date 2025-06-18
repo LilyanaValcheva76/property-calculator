1d3dc6824fb12e775520464396b80b9763c883ba
Lilyana Valcheva <lilyana.valcheva@Lilyana-Valchevas-MacBook-Pro.local>
2025-06-18 17:45:35 +0300

    Final tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",       // сканира файловете в src/
+    "./components/**/*.{js,ts,jsx,tsx}" // ако имаш папка components/
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
